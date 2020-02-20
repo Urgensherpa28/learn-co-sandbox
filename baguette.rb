89641bbdb4cf6b0d402c560a1dc1cb11508a64bf
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-20 16:30:43 +0000

    changes

---
+++ baguette.rb
@@ -0,0 +1,9 @@
+def divide_baguette_evenly(baguette)
+  baguette_length = mesaure(baguette)
+  even_length = baguette_length % n 
+  collection = []
+  
+  while baguette_length >= even_length
+  
+  
+end
